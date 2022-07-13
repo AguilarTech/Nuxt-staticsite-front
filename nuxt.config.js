2b1be068759366a1b7befc40ccfd548a11ea59b6
Daniel Aguilar <daniel@aguilartech.io>
2022-07-13 21:51:52 +1000

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,20 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://localhost:1337/api',
+    debug: true,
+  },
+
+  publicRuntimeConfig: {
+    axios: {
+      browserBaseURL: process.env.BROWSER_BASE_URL,
+    },
+  },
+
+  privateRuntimeConfig: {
+    axios: {
+      baseURL: process.env.BASE_URL,
+    },
   },
 
   strapi: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,38 @@
 <template>
-  <Tutorial />
+  <div class="container">
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <ul v-else>
+      These are the cards:
+      <li v-for="card in cards" :key="card.id">
+        {{ card.attributes.Title }}
+        {{ card.attributes.body }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'IndexPage',
+  name: 'App',
+  data() {
+    return {
+      cards: [],
+      error: null,
+    }
+  },
+
+  async fetch() {
+    try {
+      const response = await this.$axios.get('/cards')
+      this.cards = response.data.data
+    } catch (error) {
+      this.error = error
+      console.error(error)
+    }
+  },
 }
 </script>
